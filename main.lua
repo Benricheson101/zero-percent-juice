d832913f867de2d20684c327104554f02c863c44
Ben Richeson <benricheson101@gmail.com>
2026-01-28 11:49:42 -0500

    add build script

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ function love.load()
 end
 
 function love.draw()
-
+  love.graphics.print('Hello World', 400, 300)
 end
 
 function love.update(dt)
@@ -11,7 +11,12 @@ function love.update(dt)
 end
 
 function love.keypressed(key, keyCode, isRepeat)
+  print('key pressed ' .. key .. ' key code ' .. keyCode .. ' is repeat ' .. (isRepeat and 'true' or 'false'))
 
+  print(
+    'is shift + 4 ($) down: ',
+      key == '4' and love.keyboard.isDown('lshift')
+  )
 end
 
 function love.keyreleased(key, keyCode)
