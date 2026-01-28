322ec8d4e56f60f8e4dd56f42c63680080c5489c
Stephen Duffy <duffysd@sunypoly.edu>
2026-01-28 09:46:29 -0500

    boiler plate functions

---
+++ main.lua
@@ -0,0 +1,20 @@
+function love.load()
+
+end
+
+function love.draw()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.keypressed(key, keyCode, isRepeat)
+
+end
+
+function love.keyreleased(key, keyCode) 
+
+
+end
