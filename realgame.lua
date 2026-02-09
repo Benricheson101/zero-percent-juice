cf87c14852493eecc7a8faa7f63e5fd76abce8e9
Quintin Weigand <quinnweigand178@gmail.com>
2026-02-09 09:33:46 -0500

    updated realgame.lua

--- realgame.lua
+++ realgame.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 local Player = require("player")
 local Camera = require("camera")
 local Sound = require("sound")
@@ -198,31 +197,3 @@ function love.draw()
 		love.graphics.printf("Press ENTER to submit", boxX, boxY + 190, boxW, "center")
 	end
 end
-=======
-local M = {}
-
-local constants = require('constants')
-local count = 0
-function M.draw()
-    love.graphics.clear(constants.colors.black)
-
-    local text = 'game game game game'
-
-    love.graphics.setFont(constants.fonts.title)
-    love.graphics.printf(
-        text,
-        0,
-        math.floor(WindowHeight / 2)
-            - math.floor(constants.fonts.title:getHeight() / 2),
-        WindowWidth,
-        'center'
-    )
-    count = count + 1
-    if count > 300 then
-        count = 0
-        Screen = 'upgrades'
-    end
-end
-
-return M
->>>>>>> architectural-spike
