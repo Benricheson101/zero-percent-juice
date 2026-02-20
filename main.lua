e95f36b3498b6a80e7b74f689a3443125784ad74
Stephen Duffy <143434577+Stephen-Duffy-SunyPoly@users.noreply.github.com>
2026-02-20 10:03:53 -0500

    implment ui scalling system (#2)
    * implment ui scalling system
    
    * changes
    
    * additanl changes
    
    * actaual type docs

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
 --main file here
+local Ui = require("util.ui")
+
+
+function love.load()
+    -- Initialize the Ui scaling factor
+    Ui:reload()
+    --tmp
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+    love.window.updateMode(w,h, {resizable=true})
+end
+
+
+
+function love.resize(w,h)
+    --whe the window is resized, update the Ui scaling factor
+    Ui:reload()
+end
