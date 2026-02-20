9397a3bef8717a660abb0e25cb406d9aadf67498
Ben Richeson <benricheson101@gmail.com>
2026-02-20 13:03:05 -0500

    fix: wrong self param in love2d callbacks

--- scenes/example.lua
+++ scenes/example.lua
@@ -9,4 +9,12 @@ function ExampleScene:enter()
     print("Entered example scene")
 end
 
+function ExampleScene:draw()
+    love.graphics.print("Click to switch to game scene", 400, 300)
+end
+
+function ExampleScene:mousepressed()
+    self.scene_manager:transition('game')
+end
+
 return ExampleScene
