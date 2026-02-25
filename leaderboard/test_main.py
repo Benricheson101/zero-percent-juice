06464e2dfadb7f923cb95bd75e26225430db469e
Quintin Weigand <quinnweigand178@gmail.com>
2026-02-25 11:15:13 -0500

    added new tests to test trailing whitespace

--- leaderboard/test_main.py
+++ leaderboard/test_main.py
@@ -93,3 +93,39 @@ def test_long_name(client):
     assert response.status_code == 200
     data = response.json()
     assert len(data[0]["name"]) == 10000
+
+
+def test_trim_leading_space(client):
+    response = client.post("/score", json={"name": " Alice", "score": 100})
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]["name"] == "Alice"
+
+
+def test_trim_trailing_space(client):
+    response = client.post("/score", json={"name": "Alice ", "score": 100})
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]["name"] == "Alice"
+
+
+def test_trim_both_spaces(client):
+    response = client.post("/score", json={"name": " Alice ", "score": 100})
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]["name"] == "Alice"
+
+
+# For now the db allows for the same name (spelling, case, everything)
+# hence Alice can be entered twice (this is a design decision I will stick
+# with until I inevitavly decide another path)
+def test_trim_prevents_duplicate(client):
+    client.post("/score", json={"name": "Alice", "score": 100})
+    client.post("/score", json={"name": " Alice", "score": 200})
+    response = client.get("/leaderboard")
+    data = response.json()
+    assert len(data) == 2
+    assert data[0]["name"] == "Alice"
+    assert data[0]["score"] == 200
+    assert data[1]["name"] == "Alice"
+    assert data[1]["score"] == 100
