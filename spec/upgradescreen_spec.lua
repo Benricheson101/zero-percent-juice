fb49036021ba84239d5375f464f0f88e991a69da
Stephen Duffy <143434577+Stephen-Duffy-SunyPoly@users.noreply.github.com>
2026-03-06 09:37:54 -0500

    Upgrades screen (#11)
    * create base for upgrade screen
    
    * start building upgrade screen tank
    
    * finsh the tank
    
    * jittery pressure needle
    
    * upgrade slots
    
    * upgrade screen details
    
    * create upgrade system and add placeholder upgrades
    
    * money!
    
    * get upgrade by name function
    
    * add tests for upgrade screen
    
    * move love graphics loading out of global
    
    ---------
    
    Co-authored-by: Ben Richeson <benricheson101@gmail.com>

---
+++ spec/upgradescreen_spec.lua
@@ -0,0 +1,80 @@
+local player = require('player')
+local UpgradeScreen = require('scenes.upgradeScreen')
+local upgrades = require('upgrades')
+local SceneManager = require('renderer.scenemanager')
+local Ui = require('util.ui')
+local GameScene = require('scenes.game')
+
+describe('UpgradeScreen', function()
+    UpgradeScreen.scene_manager = SceneManager:new({
+        upgrade = UpgradeScreen,
+        game = GameScene,
+    })
+
+    before_each(function()
+        UpgradeScreen.scene_manager:transition('upgrade')
+        player.money = 1000
+
+        for _, upgrade in ipairs(upgrades) do
+            upgrade.level = 0
+        end
+    end)
+
+    it('decreases player money when buying an upgrade', function()
+        local startMoney = player.money
+        local x, y, _ = UpgradeScreen.calculateUpgradePosition(2)
+        local upgrade = upgrades[2]
+
+        assert.is.equal(startMoney, player.money)
+
+        local upgradeCost = upgrade:getPrice()
+        UpgradeScreen:mousepressed(x + 1, y + 1, 1)
+        assert.is.equal(startMoney - upgradeCost, player.money)
+        startMoney = startMoney - upgradeCost
+
+        upgradeCost = upgrade:getPrice()
+        UpgradeScreen:mousepressed(x + 1, y + 1, 1)
+        assert.is.equal(startMoney - upgradeCost, player.money)
+        startMoney = startMoney - upgradeCost
+    end)
+
+    it('increases upgrade level when purchasing', function()
+        local x, y, _ = UpgradeScreen.calculateUpgradePosition(2)
+        local upgrade = upgrades[2]
+
+        assert.is.equal(0, upgrade:getLevel())
+
+        UpgradeScreen:mousepressed(x + 1, y + 1, 1)
+        assert.is.equal(1, upgrade:getLevel())
+
+        UpgradeScreen:mousepressed(x + 1, y + 1, 1)
+        assert.is.equal(2, upgrade:getLevel())
+    end)
+
+    it("doesn't allow buying upgrades that cost more than balance", function()
+        local x, y, _ = UpgradeScreen.calculateUpgradePosition(2)
+        local upgrade = upgrades[2]
+
+        while player.money > upgrade:getPrice() do
+            UpgradeScreen:mousepressed(x + 1, y + 1, 1)
+        end
+
+        local level = upgrade:getLevel()
+        UpgradeScreen:mousepressed(x + 1, y + 1, 1)
+        assert.is.equal(level, upgrade:getLevel())
+    end)
+
+    it('switches to game scene when gauge is clicked', function()
+        assert.is.equal(
+            UpgradeScreen.scene_manager.active,
+            UpgradeScreen.scene_manager.scenes.upgrade
+        )
+
+        local x, y = Ui:scaleCoord(640, 500)
+        UpgradeScreen:mousepressed(x, y, 1)
+        assert.is.equal(
+            UpgradeScreen.scene_manager.active,
+            UpgradeScreen.scene_manager.scenes.game
+        )
+    end)
+end)
