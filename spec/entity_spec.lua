bb8f8d45788c37296a0194d930f7dcf9e6ffa323
JavierCrespoLaw <crespoj1@sunypoly.edu>
2026-03-30 14:00:20 -0400

    Unit testing for entity

---
+++ spec/entity_spec.lua
@@ -0,0 +1,43 @@
+local Entity = require('entity')
+
+describe('Entity', function()
+
+    describe('new', function()
+        it('creates an entity with the correct position, velocity, and image', function()
+
+            local entity = Entity.new {
+                posX = 1000,
+                posY = 800,
+                velocityX = 100,
+                image = 'images/Obstacle.png'
+            }
+
+            assert.are.equal(1000, entity.posX)
+            assert.are.equal(800, entity.posY)
+            assert.are.equal(100, entity.velocityX)
+            assert.are.equal('images/Obstacle.png', entity.image)
+
+        end)
+    end)
+
+    describe('update', function() 
+
+        it('updates the position and the rotation of the entity based on the velocity and deltaTime', function() 
+
+            local entity = Entity.new {
+                posX = 1000,
+                posY = 800,
+                velocityX = 100,
+                image = 'images/Obstacle.png'
+            }
+
+            entity:update(1)
+
+            assert.are.equal(900, entity.posX)
+            assert.are.equal(1.5, entity.rotation)
+
+        end)
+
+    end)
+
+end)
