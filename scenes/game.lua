2a0823fdfe09306ba4356865d586ce7163a3a845
Javier Crespo-Law <122939569+JavierCrespoLaw@users.noreply.github.com>
2026-03-30 09:26:03 -0400

    Coin Spawning and Spawner Refactoring (#19)
    * Coin Spawning and Spawner Refactoring
    
    - Changed ObstacleSpawner and Obstacle to ObjectSpawner and Object, making them more generalized so that they can be used for more than just obstacles.
    
    - Added in a coin spawner that functions similarly to the obstacle spawner. When the player comes in contact with a coin, their money total increases.
    
    * added parenthesis for readability
    
    * renamed "object" to "entity" for clarity
    
    renamed the base object class to entity to avoid OOP naming confusion. I
    also replaced all instances, uses, and comments to reflect the change.
    
    * commented out unused designSizes
    
    * renamed all missed instance variable names of old ObjectSpawner
    
    ---------
    
    Co-authored-by: Quintin Weigand <quinnweigand178@gmail.com>

--- scenes/game.lua
+++ scenes/game.lua
@@ -2,10 +2,10 @@ local Scene = require('renderer.scene')
 local Player = require('player')
 local Camera = require('camera')
 local Background = require('background')
-local ObstacleSpawner = require('obstacleSpawner')
+local EntitySpawner = require('entitySpawner')
 
 local designWidth = 1280
-local designHeight = 720
+-- local designHeight = 720
 
 ---@class GameScene : Scene
 local GameScene = {}
@@ -31,9 +31,18 @@ function GameScene:new()
     Camera.load(Player)
     Background.load()
 
-    ObstacleSpawner.load {
-        baseSpawnDistance = designWidth / 2,
+    EntitySpawner = EntitySpawner:new {
+        baseSpawnDistance = designWidth,
+        spawnDistance = designWidth,
         baseVelocityX = 50,
+        image = 'images/Obstacle.png',
+    }
+
+    CoinSpawner = EntitySpawner:new {
+        baseSpawnDistance = designWidth,
+        spawnDistance = designWidth / 2,
+        baseVelocityX = 50,
+        image = 'images/Coin.png',
     }
 
     return o
@@ -42,29 +51,41 @@ end
 function GameScene:update(dt)
     Player.update(dt)
     Camera.update(dt)
-    ObstacleSpawner.update(dt)
+    EntitySpawner:update(dt)
+    CoinSpawner:update(dt)
 
-    ObstacleSpawner.updateObstacleVelocityX(Camera.getVelocityX())
-    if ObstacleSpawner.checkCollision(Player.posX, Player.posY, Player.dim) then
-        -- Camera now to handle x velocity
-        Camera.changeVelocityX(-150)
-    end
+    EntitySpawner:updateEntityVelocityX(Camera.getVelocityX())
+    CoinSpawner:updateEntityVelocityX(Camera.getVelocityX())
+    GameScene:checkCollision(Player.posX, Player.posY, Player.dim)
 end
 
 function GameScene:draw()
     love.graphics.setColor(1, 1, 1)
     Background.draw(Camera)
     Player.draw()
-    ObstacleSpawner.draw()
+    EntitySpawner:draw()
+    CoinSpawner:draw()
 end
 
 function GameScene:keypressed(key)
     Player.keypressed(key)
-    ObstacleSpawner.keypressed(key)
+    EntitySpawner:keypressed(key)
+    CoinSpawner:keypressed(key)
 end
 
 function GameScene:keyreleased(key)
     Player.keyreleased(key)
+end
+
+function GameScene:checkCollision(posX, posY, dim)
+    if EntitySpawner:checkCollision(posX, posY, dim) then
+        -- Camera now to handle x velocity
+        Camera.changeVelocityX(-150)
+    end
+
+    if CoinSpawner:checkCollision(posX, posY, dim) then
+        Player.money = Player.money + 10
+    end
 end
 
 return GameScene
