2dfb6e9b63222736df3f504496f9b8076068715f
JavierCrespoLaw <crespoj1@sunypoly.edu>
2026-04-01 11:43:21 -0400

    Entity spawner testing, powerup spawner, changes to camera x velocity
    - Added testing for entity spawner.
    - Added a powerup spawner that spawns powerups which increase your velocity when you come into contact with them.
    - Changed camera update function so that it uses the changeVelocity function, preventing it from going over the maximum x velocity.

---
+++ spec/entitySpawner_spec.lua
@@ -0,0 +1,84 @@
+local EntitySpawner = require('entitySpawner')
+
+describe('EntitySpawner', function ()
+
+    describe('new', function ()
+
+        it('creates a new instance of entitySpawner with the correct parameters', function ()
+
+            local entitySpawner = EntitySpawner:new {
+                baseSpawnDistance = 1500,
+                spawnDistance = 1000,
+                baseVelocityX = 50,
+                image = 'images/Obstacle.png',
+            }
+            
+            assert.are.equal(1500, entitySpawner.baseSpawnDistance)
+            assert.are.equal(1000, entitySpawner.spawnDistance)
+            assert.are.equal(50, entitySpawner.baseVelocityX)
+            assert.are.equal('images/Obstacle.png', entitySpawner.image)
+
+        end)
+        
+    end)
+
+    describe('update', function ()
+
+        it('Updates entities and determines if a new entity should be spawned in', function ()
+        
+            local entitySpawner = EntitySpawner:new {
+                baseSpawnDistance = 1500,
+                spawnDistance = 1000,
+                baseVelocityX = 50,
+                image = 'images/Obstacle.png',
+            }
+
+            entitySpawner:update(1)
+
+            assert.are.equal(950, entitySpawner.spawnDistance)
+        
+        end)
+        
+    end)
+
+    describe('spawn', function ()
+
+        it('creates a new entity at a specific position', function ()
+
+            local entitySpawner = EntitySpawner:new {
+                baseSpawnDistance = 1500,
+                spawnDistance = 1000,
+                baseVelocityX = 50,
+                image = 'images/Obstacle.png',
+            }
+
+            entitySpawner:spawn(200)
+
+            assert.are.equal(200, entitySpawner.entities[1].posY)
+            
+        end)
+
+    end)
+
+    describe('checkCollision', function ()
+
+        it('Checks if any entities are colliding with another object at the given dimensions', function ()
+
+            local entitySpawner = EntitySpawner:new {
+                baseSpawnDistance = 1500,
+                spawnDistance = 1000,
+                baseVelocityX = 50,
+                image = 'images/Obstacle.png',
+            }
+
+            entitySpawner:spawn(200)
+
+            local collided = entitySpawner:checkCollision(1920, 200, 10)
+
+            assert.are.equal(true, collided)
+            
+        end)
+        
+    end)
+    
+end)
